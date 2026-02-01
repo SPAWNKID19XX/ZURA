294549083c96453f35c73f33e1c479f64690066b
boris-isac <borisisac@gmail.com>
2026-02-01 16:13:12 +0000

    [UPDATE] add logs to permitions

--- server/users/permissions.py
+++ server/users/permissions.py
@@ -1,4 +1,7 @@
+import logging
 from rest_framework import permissions
+logger = logging.getLogger('zura')
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -9,10 +12,14 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, object):
+        if not object.created_by:
+            logger.error(f"DATABASE INTEGRITY ERROR: Company {object.id} has no owner!")
+            return False
+
         if view.action == 'retrieve':
             return True
-        if view.action == 'create':
-            return request.user and request.user.is_authenticated and request.user.is_seo_user
-        if view.action == 'update' or view.action == 'partial_update' or view.action == 'destroy':
-            created_by = object.created_by
-            return request.user and request.user.is_authenticated and request.user.is_seo_user and request.user.id == created_by.id
+        if view.action in ['update', 'partial_update', 'destroy']:
+            is_owner = request.user == object.created_by
+            if not is_owner:
+                logger.debug(f"Access danied for User: {request.user.id} becouse owner on Object is: {object.id}")
+            return is_owner
