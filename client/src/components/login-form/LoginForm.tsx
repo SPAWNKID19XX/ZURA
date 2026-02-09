9e41d8a4342da9603fb28113cfa5cc4d8362ec64
boris-isac <borisisac@gmail.com>
2026-02-09 15:59:54 +0000

    [ADD] labels for form login

--- client/src/components/login-form/LoginForm.tsx
+++ client/src/components/login-form/LoginForm.tsx
@@ -8,32 +8,38 @@ import styles from "./LoginForm.module.css"
 export function LoginForm() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-
+    
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        console.log('============',e);
-        console.log("Data ready to send");
-        console.log("email: ", email);
-        console.log("pass: ", password);
+        e.preventDefault()
     }
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
             <h3 className={styles.form_title}>Login Form</h3>
-            <p>
+            
+            <p className={styles.form_paragraf}>
+                <label htmlFor="email_input">Email</label>
                 <input 
+                    id="email_input"
                     type="email"  
                     placeholder="Email"
                     onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                     required 
                 />
             </p>
-            <p>
+            
+            <p className={styles.form_paragraf}>
+                <label htmlFor="password_input">Password</label>
                 <input 
+                    id="password_input"
                     type="password" 
                     placeholder="Password"
                     onChange={(e) => setPassword(e.target.value)}
+                    value={password}
                     required 
                 />
             </p>
+                
             <button type="submit">Login</button>
         </form>
     )
