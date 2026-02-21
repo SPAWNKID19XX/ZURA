a2890ea1ca09c99bcf29ebe12de40c6b2e0378b5
spawnkid1987 <borisisac@gmail.com>
2026-02-21 22:20:56 +0000

    [UPDATE] connected tanstack to my account

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,30 @@
 import { StrictMode } from 'react'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools' 
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 1. Add this import
+import { BrowserRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './api/authProvider.tsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
 
