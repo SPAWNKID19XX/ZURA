10349ba59f6a6ec59eb93de8d910165c5c7a3af0
spawnkid1987 <borisisac@gmail.com>
2026-02-21 23:47:16 +0000

    [ADD] protected provider links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import { Footer } from "./components/footer/footer"
 import { LoginPage } from './pages/login-page/LoginPage'
 import { SignupPage } from './pages/signup-page/SignupPage'
 import { MyAccountPage } from './pages/my-account-page/MyAccountPage'
+import ProtectedRoute from './api/privateRouts'
+
 function App() {
   return (  
     <>
@@ -13,7 +15,9 @@ function App() {
         <Route path="/" element={<div>Главная страница Zura</div>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/my_account" element={<MyAccountPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/my_account" element={<MyAccountPage />} />
+        </Route>
       </Routes>
       <Footer />
     </>
